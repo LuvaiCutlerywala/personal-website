913185d134d9be0f9bd7d7386fa8cc8e55aec355
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2022-10-10 13:10:48 +0100

    Started implementing the intersection observer api

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let csSectionOptions = {
+    root: document.querySelector("#cs-section"),
+    rootMargin: 0,
+    threshold: 0.75
+};
+
+function csSectionRender() {
+    
+};
+
+let csSectionObserver = new IntersectionObserver();
