6c9f75ed7c80c191f562ff73002bafa896d58adc
Luvai Cutlerywala <luvaidc@gmail.com>
2022-10-21 18:55:45 +0100

    Added renderers to the cs-section element

--- scripts.js
+++ scripts.js
@@ -1,11 +1,32 @@
-let csSectionOptions = {
-    root: document.querySelector("#cs-section"),
-    rootMargin: 0,
-    threshold: 0.75
-};
+function assignRenderers(){
 
-function csSectionRender() {
-    
-};
+    //Elements with renderers attached:
+    let csSection = document.getElementById("cs-section");
 
-let csSectionObserver = new IntersectionObserver();
+    //Observer options:
+    let csSectionOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5
+    };
+
+    //Intersection Observers:
+    new IntersectionObserver(csSectionRender, csSectionOptions).observe(csSection);
+}
+
+function csSectionRender(entries, observer) {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.style.animation = "render-background 1s ease-in-out";
+            entry.target.addEventListener("animationend", () => {
+                entry.target.style.opacity = 1;
+            });
+        }
+    });
+}
+
+//On load, the intersection observers should be assigned to each component that has one.
+window.addEventListener("load", (event) => {
+    assignRenderers();
+});
+
